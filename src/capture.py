9e1d280ad97a0f41e9c15ef787a4cbadd07fd1e5
Lina Uribe <lina8452@gmail.com>
2021-04-14 01:10:53 -0500

    Se configuran los controles para el juego, funcionalidad terminada

--- src/capture.py
+++ src/capture.py
@@ -17,8 +17,10 @@
 # --------------------------------------------------------------------------
 
 import cv2
-import pyautogui
 import numpy as np
+import pyautogui
+from directKeys import  up, left, down, right
+from directKeys import PressKey, ReleaseKey
 
 # --------------------------------------------------------------------------
 # -- Se inicializan variables ----------------------------------------------
@@ -39,6 +41,15 @@ kernel = np.ones((5, 5), np.uint8)
 colormax = np.array([30, 255, 255], np.uint8)
 colormin = np.array([20, 100, 100], np.uint8)
 
+#set the initial values for parameter to use them later in the code
+current_key = set()
+# Se establece el radio del círculo para cubrir el objeto
+radius_of_circle = 1
+# Se establece el tamaño de la ventana del marco 
+window_size = 160
+# Se define la fuente de la letra con la que se escribirá en pantalla
+font = cv2.FONT_HERSHEY_SIMPLEX
+
 # --------------------------------------------------------------------------
 # -- Lectura constante de la imagenes --------------------------------------
 # --------------------------------------------------------------------------
@@ -48,6 +59,16 @@ while True:
     # ret: la usaremos para detectar si se estan recibiendo o no imagenes
     # frame: es la imagen leida en milisegundos
     ret, frame = vc.read()
+    frame = cv2.flip(frame,1)
+    # Se declaran variables con las dimensiones de la pantalla    
+    height,width = frame.shape[:2]
+    frame = cv2.resize(frame, dsize=(width, height))
+
+    # Limites de las areas de juego Arriba, abajo, derecha e izquierda
+    up_limit = (height/2 - window_size //2)
+    down_limit = (height/2 + window_size //2)
+    left_limit = (width/2 - window_size //2)
+    right_limit = (width/2 + window_size //2)
 
     # Se aplica la función GaussianBlur para reducir el detalle de la imagen
     bluryellow_frame = cv2.GaussianBlur(frame, (5, 5), 0)
@@ -79,56 +100,68 @@ while True:
 
     # Se dibuja el rectangulo y se pinta de color amarillo con un grosor de 4
     cv2.rectangle(frame,(a,b),(a+c,b+d),(30, 255, 255),4)
-
+    # Coordenadas del centro del objeto
+    centre = (int(a+c/2),int(b+d/2))
     # Se dibuja un circulo en el centro del objeto, con grosor 6 y color rojo. El -1 es para rellenar
-    cv2.circle(frame,(int(a+c/2),int(b+d/2)),6,(0,0,100),-1)
-
+    cv2.circle(frame,centre,6,(0, 255, 0),-1)
 
     # --------------------------------------------------------------------------
-    # -- Se haya la ubicación del jugador --------------------------------------
+    # -- Se halla la ubicación del jugador --------------------------------------
     # --------------------------------------------------------------------------
-
-# -------------------- <MONO --------------------------------- 
-
-    # encuentra los contornos de la mano
-    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    # Se definen los valores iniciales del centroide y del area encerrada por el contorno
-    x, y, area = 0, 0, 0
-
-    if len(contours) != 0:
-        # Encontrar el contorno de area maxima(mano)
-        cnt = max(contours, key=lambda x: cv2.contourArea(x))
-        area = cv2.contourArea(cnt)
-
-        if area > 200:
-            # Obtener los momentos 
-            M = cv2.moments(cnt)
-            if M["m00"] == 0:
-                M["m00"] = 1
+   
+    # Para asegurarnos que todas las teclas queden liberadas al inicio del juego
+    keyPressed = False
+    keyPressed_lr = False         
             
-            # Se calcula la posición del centroide a partir de los momentos obtenidos
-            x = int(M["m10"] / M["m00"])
-            y = int(M['m01'] / M['m00'])
+    # Se establecen posiciones donde se detectarán las teclas arriba y abajo
+    if centre[1] < up_limit and centre[1] > 0 and centre[0] > left_limit and centre[0] < right_limit:
+        cv2.putText(frame ,'ARRIBA',(20,50),font,1,(255,255,255),2)
+        # Para hacer click en la tecla ARRIBA con pyautogui
+        pyautogui.press('up')
+        current_key.add(up)
+        keyPressed = True
+        keyPressed_lr=True
+    elif centre[1] > down_limit and centre[0] > left_limit and centre[0] < right_limit:
+        cv2.putText(frame,'ABAJO',(20,50),font,1,(255,255,255),2)
+        # Para hacer click en la tecla ABAJO con pyautogui
+        pyautogui.press('down')
+        current_key.add(down)
+        keyPressed = True
+        keyPressed_lr=True    
+    # Se establecen posiciones donde se detectarán las teclas derecha e izquierda
+    elif centre[1] > up_limit and centre[1] < down_limit and centre[0] < left_limit:
+        cv2.putText(frame,'IZQUIERDA',(20,50),font,1,(255,255,255),2)
+        # Para hacer click en la tecla IZQUIERDA con pyautogui
+        pyautogui.press('left')
+        keyPressed = True
+        current_key.add(left)
+    elif centre[1] > up_limit and centre[1] < down_limit and centre[0] > right_limit:
+        cv2.putText(frame,'DERECHA',(20,50),font,1,(255,255,255),2)
+        # Para hacer click en la tecla DERECHA con pyautogui
+        pyautogui.press('right')
+        keyPressed = True
+        current_key.add(right)
 
-            # Se dibuja un circulo en donde está ubicado el centroide
-            cv2.circle(frame, (x, y), 7, (255, 0, 0), 1)
+    # Se liberan las teclas que hayan sido presionadas, para seguir jugando
+    if not keyPressed and current_key!= 0:
+        for key in current_key:
+            ReleaseKey(key)
+            current_key=set()
 
-            # Se define la fuente de la letra con la que se escribirá en pantalla
-            font = cv2.FONT_HERSHEY_SIMPLEX
-
-            # Se escribe en pantalla las coordenadas del centroide y el área encerrada por el contorno
-            cv2.putText(frame, '{},{}'.format(x, y), (x + 10, y), font, 0.75, (0, 255, 0), 1, cv2.LINE_AA)
-            cv2.putText(frame, '{}'.format(area), (x + 10, y + 20), font, 0.75, (0, 255, 0), 1, cv2.LINE_AA)
-
-# -------------------- MONO/> ---------------------------------
-
+    # --------------------------------------------------------------------------
+    # -- Se dibujan las areas de juego en el frame -----------------------------
+    # --------------------------------------------------------------------------
+    
+    # Se dibuja el area para las acciones izquierda y derecha
+    frame = cv2.rectangle(frame,(width//2 - window_size //2,0),(width//2 + window_size //2,height),(0,255,0),2)    
+    # Se dibuja el area para las acciones de arriba y abajo
+    frame= cv2.rectangle(frame,(0,height//2 - window_size //2),(width,height//2 + window_size //2),(255,0,0),2)
 
     # --------------------------------------------------------------------------
     # -- Se abren las ventanas para mostrar ------------------------------------
     # --------------------------------------------------------------------------
-
-    # Se muestra la imagen captada
+        
+    # Se muestra la imagen captada con las areas de juego
     cv2.imshow("webcam", frame)
     # Se muestra la imagen captada aplicandole la mascara
     #cv2.imshow('mascara', mask)
@@ -139,7 +172,7 @@ while True:
     # -- Se detiene el juego cuando el jugador así lo decida -------------------
     # --------------------------------------------------------------------------
 
-    # Se ermina el ciclo al presionar la tecla 'q'
+    # Se termina el ciclo al presionar la tecla 'q'
     if cv2.waitKey(10)==ord('q'):
         break
 
